9481aa0b6105125af7e7a4fd0912a2038b0f0574
Christopher Hockley <christopher@marketingmaps.co.uk>
2020-04-24 16:33:20 +0100

    added addItem and removeItem to checkout page and redux

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,23 @@ export const addItemToCart = (cartItems, item) => {
 
   return [...cartItems, { ...item, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, item) => {
+  const lastCartItem = cartItems.find(
+    (cartItem) => cartItem.id === item.id && item.quantity === 1
+  );
+
+  if (!lastCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === item.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  }
+
+  return cartItems.filter((cartItem) => cartItem.id !== item.id);
+};
+
+export const clearItemFromCart = (cartItems, item) => {
+  return cartItems.filter((cartItem) => cartItem.id !== item.id);
+};
